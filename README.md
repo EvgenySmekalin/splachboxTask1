de1fea4f1889c6bce7ff3bf3b6bc19af4c42a9c7
Evgeny Smekalin <bakaExpire@mail.ru>
2019-08-09 12:58:11 +0700

    First task initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # splachboxTask1
+
+### function printBooleanArray prints a grid of boolean array
+
+To this run code print in console
+
+`php index.php`
+
+*enter columns number and press Enter to print a grid of boolean array*
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+function printBooleanArray() 
+{
+    $arrayDimension = (int) readline("Columns number: ");
+
+    if (gettype($arrayDimension) !== "integer")  {
+        exit("Array dimension mast be integer");
+    }
+
+    if ($arrayDimension < 0) {
+        exit("Can't buld array with negative number of columns");
+    }
+
+    if ($arrayDimension == 0) {
+        exit("Array is empty");
+    }
+
+    foreach (range(1, $arrayDimension) as $row) {
+        foreach(range(1, $arrayDimension) as $column) {
+            echo rand(0, 1) . ' ';
+        }
+
+        echo "\n";
+    }
+}
+
+printBooleanArray();
